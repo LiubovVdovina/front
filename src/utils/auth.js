3fad311dc93672833c002173c4c5616cc2e023ea
Liubov Kushko <lyuba-0258@yandex.ru>
2023-09-07 20:58:56 +0700

    initial

---
+++ src/utils/auth.js
@@ -0,0 +1,45 @@
+export const BASE_URL = 'https://api.mestollogy.nomoredomainsicu.ru';
+
+function getResponseData(res) {
+  if(res.ok) {
+    return res.json()
+  } else {
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+}
+
+export function register(email, password) {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    credentials: 'include',
+    headers: {
+      "Content-Type": "application/json" 
+    },
+    body: JSON.stringify({password, email})
+  })
+  .then((res) => getResponseData(res))
+}
+
+export function authorize(email, password) {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    credentials: 'include',
+    headers: {
+      "Content-Type": "application/json" 
+    },
+    body: JSON.stringify({password, email})
+  })
+  .then((res) => getResponseData(res))
+  .then((data) => {
+    return data;
+  })
+}
+export function checkToken() {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then((res) => getResponseData(res))
+}
